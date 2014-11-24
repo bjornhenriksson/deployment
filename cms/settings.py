cf33646cbb234b5edd4ce4c9b1cdd045ec1f6baa
Björn Henriksson <bjarna2@gmail.com>
2014-11-24 14:30:55 +0100

    changed local postgres

--- cms/settings.py
+++ cms/settings.py
@@ -29,7 +29,6 @@ TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = (
@@ -66,6 +65,7 @@ DATABASES = {
     }
 }
 
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
@@ -87,7 +87,7 @@ STATIC_URL = '/static/'
 
 
 # Parse database configuration from $DATABASE_URL
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config(default='postgresql://bjorn@localhost/bjorn')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
