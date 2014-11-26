c91ef9e7cd1ac363d52d49316d36822a743ba3f6
Björn Henriksson <bjarna2@gmail.com>
2014-11-26 13:33:30 +0100

    added medium editor

--- page/static/page/js/functions.js
+++ page/static/page/js/functions.js
@@ -1,5 +1,13 @@
 jQuery(document).ready(function ($) {
 
+    var editor = new MediumEditor('.edit-p');
+    $(".js-form").submit(function( event ) {
+      event.preventDefault();
+      var contentObj = editor.serialize();
+      $(".js-content-hidden").val(contentObj['element-0'].value);
+      this.submit();
+    });
+
     $('textarea').autosize();
 
 });   
